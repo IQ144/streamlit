99297e3cff3936c1c79d4728ca71e5b6609e52a9
IQ144 <154084503+IQ144@users.noreply.github.com>
2023-12-18 00:14:06 +0600

    KKKKKKKKKKKKKKKKKKKKKK
    LLLLLLLLLLLLLLLLLLLLLLLLLLL

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit
+streamlit.set_page_config(page_title="I LOVE PYTHON", layout="wide")
+for _ in range(10):
+    streamlit.write("I LIKE TO KNOW PYTHON")
+    streamlit.write("I WANT TO BE A AI DEVELOPER")
+
